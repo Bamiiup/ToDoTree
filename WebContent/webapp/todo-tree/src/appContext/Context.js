7817362ae6b9a0212b05c1bb819a7c0f16931d9c
AAAndrey <bamiiup3456@gmail.com>
2017-08-30 02:08:21 +0300

    Revert "Added registration,  authentication and welcome page for client"
    This reverts commit 8dbe6b01c4f66ca4daae88106ce4f806866d38ee.

--- WebContent/webapp/todo-tree/src/appContext/Context.js
+++
@@ -1,9 +0,0 @@
-import UserService from "./../services/UserService";
-
-const startUrl = "http://localhost:8080/ToDoTree/";
-
-const userService = new UserService();
-
-userService.setStartUrl(startUrl);
-
-export {userService};
