78023b4906aa60616f79cfa36922f87de2aae274
AAAndrey <bamiiup3456@gmail.com>
2017-10-22 16:51:01 +0300

    Added complete maven build process

--- webapp/todo-tree/src/appContext/Context.js
+++ webapp/todo-tree/src/appContext/Context.js
@@ -7,7 +7,7 @@ import mainReducer from './../store/MainReducer';
 const store = createStore(mainReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-const startUrl = "http://localhost:8080/ToDoTree/";
+const startUrl = "./";
 
 const userService = new UserService(startUrl);
 const todoService = new TodoService(startUrl);
